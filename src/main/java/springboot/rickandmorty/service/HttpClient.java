90f342bead4e3b2fd48ea6c3e5a798eeef00a045
X3NON-135 <immort4ld3momo@gmail.com>
2022-11-25 18:19:05 +0000

    optimised usage of beans

--- src/main/java/springboot/rickandmorty/service/HttpClient.java
+++ src/main/java/springboot/rickandmorty/service/HttpClient.java
@@ -1,22 +1,27 @@
 package springboot.rickandmorty.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.springframework.stereotype.Component;
-import springboot.rickandmorty.config.ObjectMapperConfig;
 
 @Component
 public class HttpClient {
-    private final CloseableHttpClient httpClient = HttpClients.createDefault();
-    private final ObjectMapperConfig objectMapper = new ObjectMapperConfig();
+    private final CloseableHttpClient closeableHttpClient;
+    private final ObjectMapper objectMapper;
+
+    public HttpClient(CloseableHttpClient closeableHttpClient,
+                      ObjectMapper objectMapper) {
+        this.closeableHttpClient = closeableHttpClient;
+        this.objectMapper = objectMapper;
+    }
 
     public <T> T get(String url, Class<T> clazz) {
         HttpGet request = new HttpGet(url);
-        try (CloseableHttpResponse response = httpClient.execute(request)) {
-            return objectMapper.call().readValue(response.getEntity().getContent(), clazz);
+        try (CloseableHttpResponse response = closeableHttpClient.execute(request)) {
+            return objectMapper.readValue(response.getEntity().getContent(), clazz);
         } catch (IOException e) {
             throw new RuntimeException("Can't fetch info from URL " + url, e);
         }
