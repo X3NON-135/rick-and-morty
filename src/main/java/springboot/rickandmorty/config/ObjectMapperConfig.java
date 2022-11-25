71f67c1053c14ba1db4832401e4510e6ed4fc525
X3NON-135 <immort4ld3momo@gmail.com>
2022-11-25 18:18:37 +0000

    added ObjectMapperConfig beans

--- src/main/java/springboot/rickandmorty/config/ObjectMapperConfig.java
+++ src/main/java/springboot/rickandmorty/config/ObjectMapperConfig.java
@@ -2,13 +2,20 @@ package springboot.rickandmorty.config;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ObjectMapperConfig {
     @Bean
-    public ObjectMapper call() {
+    public CloseableHttpClient closeableHttpClient() {
+        return HttpClients.createDefault();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return objectMapper;
